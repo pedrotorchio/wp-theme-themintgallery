db4eab6a956593f84cba9e1743c293b442a446c8
Pedro Torchio <pedrotorchio@gmail.com>
2019-01-11 13:18:13 -0300

    css reset

--- src/App.vue
+++ src/App.vue
@@ -14,22 +14,6 @@ export default {
       router-view
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="scss" src="css-reset-and-normalize/scss/reset-and-normalize.scss"></style>
+<style lang="sass" src="@/styles/global.sass"></style>
+<style lang="sass" src="@/styles/root.sass"></style>
