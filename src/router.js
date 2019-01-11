2885224e7849b18598130108fa7e2bdd8c724383
Pedro Torchio <pedrotorchio@gmail.com>
2019-01-11 12:57:42 -0300

    removing conflicting host base url

--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ Vue.use(Router);
 
 export default new Router({
   mode: "history",
-  base: process.env.BASE_URL,
+  base: '', //process.env.BASE_URL,
   routes: [
     { path: '/', name: 'home', component: Home },
     { path: '/about', name: 'about', component: About },
