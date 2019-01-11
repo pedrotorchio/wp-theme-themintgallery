f2c8cd7fee512ddbd202080a9aff79ce5a9b6c10
Pedro Torchio <pedrotorchio@gmail.com>
2019-01-11 14:53:56 -0300

    header with components and animation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,11 @@
 const menuStructure = __INITIAL_DATA__.mainMenuStructure || [];
 const socialStructure = __INITIAL_DATA__.socialMenuStructure || [];
 
+import '@/components/svg/Facebook'
+import '@/components/svg/Twitter'
+import '@/components/svg/Behance'
+import '@/components/svg/Instagram'
+
 export default {
     name: 'AppHeader',
     data: () => ({
@@ -20,18 +25,15 @@ export default {
                         li( v-for = "(parent, i) in menuStructure" :key = "parent.ID" )
                             router-link( :to = "parent.url" ) {{ parent.title }}
 
-                            ul.dropdown( v-if = "parent.children.length > 0")
+                            ul( v-if = "parent.children.length > 0")
                                 li( v-for = "(child, i) in parent.children" :key = "child.ID" )
                                     router-link( :to = "child.url" ) {{ child.title }}
 
                 nav.social
                     ul
                         li( v-for = "(parent, i) in socialStructure" :key = "parent.ID" )
-                            router-link( :to = "parent.url" ) {{ parent.title }}
-
-                            ul( v-if = "parent.children.length > 0")
-                                li( v-for = "(child, i) in parent.children" :key = "child.ID" )
-                                    router-link( :to = "child.url" )
+                            a( :href = "parent.url" )
+                                svgicon( :name = "parent.title" :original = "true" )
                                         
 
 </template>
@@ -51,58 +53,31 @@ header
     .logo
         width: $height
         height: 100%
+        background: green
         flex: 0 0 auto
     .navigation
         flex: 1 1 auto
+        background: red
         display: flex
         justify-content: flex-end
 
-#main-menu
-    flex: 1 0 auto
-    text-align: right
-    padding-right: 32px
+        #main-menu
+            flex: 1 0 auto
+        .social
+            flex: 0 0 auto
 
-.social
-    flex: 0 0 auto
+            ul
+                line-height: $height
+                height: $height
+            li
+                $size: 32px
+                display: inline-block
+                margin: 0 $size/4
 
-
-ul
-    height: $height
-    display: flex
-    align-items: center
-    justify-content: flex-end
-    top: 40%
-    top: calc(50% - .5em)
-li
-    $size: 32px
-    display: inline-block
-    margin: 0 $size/4
-    position: relative
-    
-    &>ul
-        position: absolute
-        right: 0
-        top: 0
-        visibility: hidden
-        opacity: 0
-        transition-property: opacity, transform
-        transition-duration: .2s
-        transform: translateY(-#{$size--font-text})
-        display: block
-        padding-top: $size--font-text * 2
-
-    &:hover
-        &>ul
-            transform: translateY(0)
-            visibility: visible
-            opacity: 1
-
-    a
-        display: block
-        white-space: nowrap
-    svg
-        width: $size
-        height: $size
-
+                a
+                    display: block
+                svg
+                    width: $size
+                    height: $size
 </style>
 
