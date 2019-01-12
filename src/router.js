90646cb256d05fd056736ee6c2a7be7fee5231d0
Pedro Torchio <pedrotorchio@gmail.com>
2019-01-12 12:22:38 -0300

    nested artists routing

--- src/router.js
+++ src/router.js
@@ -1,20 +1,20 @@
 import Vue from "vue";
 import Router from "vue-router";
+import TransparentView from './views/Transparent';
 
 Vue.use(Router);
 
 function asyncView(view) {
   return () => import(`./views/${view}`);
 }
-
 export default new Router({
   mode: "history",
   base: '', //process.env.BASE_URL,
   routes: [
     { path: '/', name: 'home', component: asyncView('Home') },
 
-    { path: '/artists', children: [
-      { path: '', component: asyncView('Artists') },
+    { path: '/artists', component: TransparentView, children: [
+      { path: '', name: 'Artists', component: asyncView('Artists') },
       { path: ':slug', component: asyncView('Artist') }
     ]}
   ]
---
+++ src/views/Artists.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+    name: 'Artists',
+    data: () => ({
+        artists: []
+    }),
+    created() {
+        
+    }
+}
+</script>
+<template lang="pug">
+    div
+        h1 testando
+</template>
+<style lang="sass" scoped>
+    #Artists-root
+        
+</style>
