146ff56c41e172dbbb9f0277e1a6f0774aa1a1c0
Pedro Torchio <pedrotorchio@gmail.com>
2019-01-12 11:58:17 -0300

    artists hierarchy routes

--- src/router.js
+++ src/router.js
@@ -1,15 +1,21 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
-const About = () => import( "./views/About.vue");
 
 Vue.use(Router);
+
+function asyncView(view) {
+  return () => import(`./views/${view}`);
+}
 
 export default new Router({
   mode: "history",
   base: '', //process.env.BASE_URL,
   routes: [
-    { path: '/', name: 'home', component: Home },
-    { path: '/about', name: 'about', component: About },
+    { path: '/', name: 'home', component: asyncView('Home') },
+
+    { path: '/artists', children: [
+      { path: '', component: asyncView('Artists') },
+      { path: ':slug', component: asyncView('Artist') }
+    ]}
   ]
 });
