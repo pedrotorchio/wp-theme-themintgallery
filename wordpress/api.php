728ef9e567bd4401df3d336a2ed2df3b0bf765d7
Pedro Torchio <pedrotorchio@gmail.com>
2019-01-13 16:55:21 -0300

    fixing api image distortions

--- wordpress/api.php
+++ wordpress/api.php
@@ -1,23 +1,48 @@
 <?php
 
-add_action( 'rest_api_init', 'dt_register_api_hooks' );
-function dt_register_api_hooks() {
-    foreach([
-        'artists'
+add_action( 'rest_api_init', 'editArtistData' );
 
-    ] as $type)
-        registerCFSfield($type);
-
-}
-function registerCFSfield($type) {
+function editArtistData() {
     // Add the plaintext content to GET requests for individual posts
 
     register_rest_field(
-        $type,
+        'artists',
         'CFS',
         array(
             'get_callback' => function($object, $field_name, $request) {
-                return CFS()->get(false);
+                
+                $data = CFS()->get(false);
+
+                $gallery = array_map(function ($piece) use ($data) {
+
+                    $piece['images'] = array_map(function ($imageData) use ($data) {
+                        
+                        $imageId = $imageData['image'];
+                        
+                        $image = wp_prepare_attachment_for_js($imageId);
+                        
+                        return $image;
+
+                    }, $piece['images']);
+
+                    return $piece;
+                }, $data['gallery']);
+                $data['gallery'] = $gallery;
+                
+                return $data;
+            },
+        )
+    );
+    register_rest_field(
+        'artists',
+        'acf',
+        array(
+            'get_callback' => function($object, $field_name, $request) {
+                $data = get_fields();
+                
+                $data['profile_picture'] = wp_prepare_attachment_for_js($data['profile_picture']);
+                
+                return $data;
             },
         )
     );
