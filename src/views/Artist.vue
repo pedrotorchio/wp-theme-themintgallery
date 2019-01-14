fdefb692c079523a0e0d23b5abbcf72dc142ca1a
Pedro Torchio <pedrotorchio@gmail.com>
2019-01-14 13:42:25 -0300

    artist slug props

--- src/views/Artist.vue
+++ src/views/Artist.vue
@@ -2,14 +2,36 @@
 import Artist from '@/plugins/mint-fetcher/Artist'
 
 export default {
+    name: 'Artist',
     props: {
         slug: {
             type: String,
             required: true
         }
     },
+    data: () => ({
+        artist: null
+    }),
+    methods: {
+        async fetchData() {
+            this.artist = await this.$fetcher.getArtist(this.slug)
+        }
+    },
     created() {
-        
+        this.fetchData();
     }
 }
 </script>
+<template lang="pug">
+
+    main
+        div.hero
+</template>
+<style lang="sass" scoped>
+@import '~@/styles/config'
+
+.hero
+    height: calc(100vh - #{$size--header-height})
+    
+    
+</style>
