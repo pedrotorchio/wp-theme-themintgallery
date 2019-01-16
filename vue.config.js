c2c7a03fec478aa8cc35118639862c48e46bf955
Pedro Torchio <pedrotorchio@gmail.com>
2019-01-16 20:02:55 -0300

    simplifying build file names and fixing vue public path

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,28 @@
 var publicPath;
-
+var config = {}
 
 if (process.env.NODE_ENV === 'production') {
-  publicPath = "/wp-content/themes/mint-gallery-spa";
-} else {
-  publicPath = "http://localhost:9876/";
-}
-
-
-module.exports = {
-  publicPath: publicPath,
-  // path:
-  devServer: {
-    disableHostCheck: true
+  config.publicPath = "/wp-content/themes/mint-gallery-spa/dist";
+  
+  config.chainWebpack = config => {
+    if(config.plugins.has('extract-css')) {
+      const extractCSSPlugin = config.plugin('extract-css')
+      extractCSSPlugin && extractCSSPlugin.tap(() => [{
+        filename: '[name].css',
+        chunkFilename: '[name].css'
+      }])
+    }
   }
+
+  config.configureWebpack = {
+    output: {
+      filename: '[name].js',
+      chunkFilename: '[name].js'
+    }
+  }
+
+} else {
+  config.publicPath = "http://localhost:9876/";
 }
+
+module.exports = config
