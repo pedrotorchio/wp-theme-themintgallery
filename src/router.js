2fb619d126ef01162c7176174e42b6663ff35d63
Pedro Torchio <pedrotorchio@gmail.com>
2019-01-16 18:59:45 -0300

    route initial scrolling

--- src/router.js
+++ src/router.js
@@ -17,5 +17,21 @@ export default new Router({
       { path: '', name: 'Artists', component: asyncView('Artists') },
       { path: ':slug', name: 'Artist', component: asyncView('Artist'), props: true }
     ]}
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    let scroll;
+    if (to.hash) {
+      scroll = {
+        selector: to.hash
+      }
+    } else if (savedPosition) {
+      scroll = savedPosition
+
+    } else {
+      scroll = { x: 0, y: 0 }
+
+    }
+
+    return scroll
+  }
 });
