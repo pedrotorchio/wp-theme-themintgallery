9365e3a2ac76a95922d00fa00a0848f92a5af7df
Pedro Torchio <pedrotorchio@gmail.com>
2019-01-18 13:51:36 -0300

    creating slider section

---
+++ src/components/homeView/SliderSection.vue
@@ -0,0 +1,43 @@
+<script>
+import LazyImage from 'vue-hoverable-lazy-image'
+
+function Slide (src, placeholder, title, ctaText, ctaRoute ) {
+    this.img = {
+        src,
+        "src-placeholder": placeholder,
+    }
+
+} 
+export default {
+    name: 'Slider',
+    components: { LazyImage }
+}
+</script>
+<template lang='pug'>
+    transition( appear )
+        div(
+            v-for="slide in slides" 
+            class='slide'
+            :key="slide.id" )
+
+            lazy-image( :src = "slide.img.src" :src-placeholder = "slide.img.placeholder" )
+            h4( v-if = "slide.title" ) {{ slide.title }}
+            router-link( v-if = "slide.cta" :to = "slide.cta.to" ) {{ slide.cta.text }}
+
+      
+</template>
+<style lang='sass'>
+#MyFirstSlideshow
+  .eg-slideshow
+    .eg-slide
+      .eg-slide-content
+        width: 25em
+        max-width: 80%
+        margin: 0 auto
+  .eg-slide-content
+    width: 25em
+    max-width: 80%
+    margin: 0 auto
+  
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,11 @@
-<template>
-  <div class="home">
-
-  </div>
-</template>
-
 <script>
-
+import SliderSection from '@/components/homeView/SliderSection'
 export default {
   name: "home",
-  
+  components: { SliderSection }
 };
 </script>
+
+<template lang="pug">
+  div
+</template>
